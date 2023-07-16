c40b49b704581418fc957320cdc8ab7ee4e73780
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-07-16 21:58:07 +0100

    Fix Github Pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/airreps/',
   lang: 'en-US',
   title: "AirReps",
   description: "A community for the discussion and exploration of AirPods clones.",
