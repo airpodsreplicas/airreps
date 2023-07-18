e5b18e5307c0500d094558a253ce533ec2158f30
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-07-18 18:59:38 +0100

    Create Contributors.vue
    File acts as a template

---
+++ docs/.vitepress/components/Contributors.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const contributors = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://api.github.com/repos/TowyTowy/airreps/contributors");
+    if (response.ok) {
+      const data = await response.json();
+      contributors.value = data.map((contributor) => ({
+        name: contributor.login,
+        avatar: contributor.avatar_url,
+      }));
+    }
+  } catch {
+    /* ... */
+  }
+});
+</script>
+
+<template>
+  <div class="contributors-container">
+    <hr class="divider" />
+    <h2 id="contributors" style="font-size: 24px; line-height: 32px; letter-spacing: -.02em; font-weight: 600">Contributors</h2>
+    <div class="users" flex="~ wrap gap2" justify-center>
+      <a v-for="{ name, avatar } of contributors" :key="name" :href="`https://github.com/${name}`" m-0 rel="noopener noreferrer" :aria-label="`${name} on GitHub`" :data-tooltip="name" data-position="top" class="tooltip">
+        <img class="user" loading="lazy" :src="avatar" width="50" height="50" :alt="`${name}'s avatar`" />
+      </a>
+    </div>
+  </div>
+</template>
