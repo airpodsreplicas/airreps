e956f23cd2606b234b6c31f40131b9edc165d922
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-10-18 13:24:10 +0100

    💡 (index.ts): Add a comment to ignore TypeScript error for Vue component import.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import Theme from "vitepress/theme";
 import "./styles/vars.css";
 import "./styles/main.css"
+// @ts-ignore - Vue component import
 import Contributor from '../components/Contributors.vue';
 
 export default {
