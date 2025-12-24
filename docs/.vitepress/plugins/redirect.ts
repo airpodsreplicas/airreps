56a67918440c6c5195c71179137c76817b356c7c
TowyTowy <towy@airreps.link>
2025-12-24 05:17:37 +0100

    Remove vercel.json

--- docs/.vitepress/plugins/redirect.ts
+++ docs/.vitepress/plugins/redirect.ts
@@ -35,29 +35,29 @@ export function redirectPlugin(): Plugin {
       server.middlewares.use((req, res, next) => {
         const url = req.url?.split('?')[0] || ''
         const redirect = redirects[url]
-        
+
         if (redirect) {
           res.writeHead(301, { Location: redirect })
           res.end()
           return
         }
-        
+
         next()
       })
     },
     writeBundle(options, bundle) {
       // Write redirect files after bundle is written
       const outDir = options.dir || 'dist'
-      
+
       Object.entries(redirects).forEach(([from, to]) => {
         // Remove leading slash and create directory structure
         const cleanPath = from.replace(/^\//, '').replace(/\/$/, '')
         const indexPath = join(outDir, cleanPath, 'index.html')
         const dir = dirname(indexPath)
-        
+
         // Create directory if it doesn't exist
         mkdirSync(dir, { recursive: true })
-        
+
         // Write redirect HTML file
         writeFileSync(indexPath, redirectHTML(to), 'utf-8')
       })
